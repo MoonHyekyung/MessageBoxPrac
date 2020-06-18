9e193f2cca75da07edf480b0cc59af2e5162fb76
MoonHyekyung <moonsoft02@gmail.com>
2020-06-18 14:07:21 +0900

    초기 커밋.

---
+++ MessageBoxPrac/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MessageBoxPrac
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("내용");
+            MessageBox.Show("내용","제목");
+            MessageBox.Show("내용", "제목",MessageBoxButtons.AbortRetryIgnore); // 중단, 다시, 무시
+        }
+    }
+}
